b227d329172830cd3738ed168d6f694e6ed3e163
Ryfan Aditya Indra <ryfandotnet@gmail.com>
2017-05-01 22:35:54 +0700

    Perubahan Database

--- database/db_kkp.sql
+++ database/db_kkp.sql
@@ -1,10 +1,20 @@
-﻿# Host: localhost  (Version 5.5.5-10.1.19-MariaDB)
-# Date: 2017-04-28 17:31:23
-# Generator: MySQL-Front 6.0  (Build 1.110)
+﻿# Host: localhost  (Version: 5.5.5-10.1.21-MariaDB)
+# Date: 2017-05-01 22:35:17
+# Generator: MySQL-Front 5.2  (Build 5.66)
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
+/*!40101 SET SQL_MODE='NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
+/*!40103 SET SQL_NOTES='ON' */;
+
+USE `db_kkp`;
 
 #
-# Structure for table "file"
+# Source for table "file"
 #
 
 DROP TABLE IF EXISTS `file`;
@@ -14,22 +24,21 @@ CREATE TABLE `file` (
   `file_name` varchar(255) DEFAULT NULL,
   `file_url` varchar(255) DEFAULT NULL,
   `file_size` float DEFAULT NULL,
-  `file_source` varchar(255) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
   `tgl_upload` date DEFAULT NULL,
   `status` enum('0','1') DEFAULT NULL,
   `keterangan` varchar(255) DEFAULT NULL,
-  `jumlah_dekripsi` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_file`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 #
 # Data for table "file"
 #
 
+INSERT INTO `file` VALUES (28,'ryfan','44155-uts_mppl.docx','file_encrypt/44155-uts_mppl.docx',34488,'dXRz','2017-05-01','0','UTS MPPL');
 
 #
-# Structure for table "users"
+# Source for table "users"
 #
 
 DROP TABLE IF EXISTS `users`;
@@ -47,6 +56,10 @@ CREATE TABLE `users` (
 # Data for table "users"
 #
 
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('ryfan','79e779bb0f878043e7c396229df8d5f1','Ryfan Aditya','Project Manager','2017-04-28 15:48:55','2017-04-28 17:30:39');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+INSERT INTO `users` VALUES ('ryfan','79e779bb0f878043e7c396229df8d5f1','Ryfan Aditya','Project Manager','2017-04-28 15:48:55','2017-05-01 22:25:53');
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
